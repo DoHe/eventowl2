e3ddf6e5fc5382a2470c9dcbe06ed78b24b8008e
DoHe <wahlholz@gmx.de>
2018-05-05 20:46:18 +0200

    Fix printing

--- concertowl/management/commands/update_events.py
+++ concertowl/management/commands/update_events.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         artists = [artist.name for artist in Artist.objects.all()]
         locations = {'{},{}'.format(u.city.lower(), u.country.lower()) for u in UserProfile.objects.all()}
         print("Getting events for {} artists in {} locations...".format(len(artists), len(locations)))
-        events = get_events_for_artists(artists, locations)
+        events = list(get_events_for_artists(artists, locations))
         print("Adding {} events...".format(len(events)))
         for event in events:
             add_event(event)
