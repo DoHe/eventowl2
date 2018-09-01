f2590d50854d447dcd3aaf4e83fa099db010749e
DoHe <wahlholz@gmx.de>
2018-09-01 14:36:51 +0200

    Filter events for location

--- concertowl/views.py
+++ concertowl/views.py
@@ -32,6 +32,7 @@ class Events(View):
             events = events.filter(artists__subscribers__profile__uuid=user_uuid)
         else:
             events = events.filter(artists__subscribers__id=request.user.id)
+        events.filter(city__iexact=request.user.profile.city)
         events = events.order_by('start_time').distinct()
 
         if event_format == 'ical':
