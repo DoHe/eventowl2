3fab9bf30431752c765ccb01241aad5e0ec8dd5e
dohe <wahlholz@gmx.de>
2020-04-05 14:42:22 +0200

    Protect admin accounts

--- concertowl/management/commands/clean.py
+++ concertowl/management/commands/clean.py
@@ -7,9 +7,12 @@ from django.core.management import BaseCommand
 from concertowl.models import Event, Notification
 
 
-def clean(model, field, days):
+def clean(model, field, days, exceptions={}):
     time_ago = datetime.now() - timedelta(days=days)
-    model.objects.filter(**{field + '__lt': time_ago}).delete()
+    old = model.objects.filter(**{field + '__lt': time_ago})
+    for field, values in exceptions.items():
+        old = old.exclude(**{field + '__in': values})
+    old.delete()
 
 
 class Command(BaseCommand):
@@ -17,6 +20,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         clean(Event, 'start_time', 14)
-        clean(User, 'last_login', 30)
+        clean(User, 'last_login', 30, {
+            'username': [
+                'eventowladmin',
+                'eventowl2admin',
+                'derdot'
+            ]
+        })
         clean(Notification, 'created', 30)
         LogEntry.objects.all().delete()
